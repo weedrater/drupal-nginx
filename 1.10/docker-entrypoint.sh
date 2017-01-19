f11e16256d28af840f59273014dce1a2e6b19d1e
Marc Arbesman <marbesman@gmail.com>
2017-01-19 09:00:38 -0500

    Self Signed Cert is created by  default and available on port 443

--- 1.10/docker-entrypoint.sh
+++ 1.10/docker-entrypoint.sh
@@ -18,6 +18,7 @@ fi
 
 # Copy default nginx config.
 if [[ ! "$(ls -A /etc/nginx/conf.d)" ]]; then
+	echo " --  copying drupal nginx configs"
     cp /opt/drupal${DRUPAL_VERSION}.conf /etc/nginx/conf.d/
 fi
 
@@ -43,25 +44,31 @@ DH="/etc/nginx/external/dh.pem"
 
 if [ ! -e "$DH" ]
 then
-  echo ">> seems like the first start of nginx"
-  echo ">> doing some preparations..."
+  echo " --  seems like the first start of nginx so generating certs for SSL"
   echo ""
-
-  echo ">> generating $DH with size: $DH_SIZE"
-  openssl dhparam -out "$DH" $DH_SIZE
+  cp /etc/nginx/external/ssl.conf /etc/nginx/conf.d
+  echo " --  generating $DH with size: $DH_SIZE"
+  openssl dhparam -out "$DH" $DH_SIZE 
 fi
 
 if [ ! -e "/etc/nginx/external/cert.pem" ] || [ ! -e "/etc/nginx/external/key.pem" ]
 then
-  echo ">> generating self signed cert"
+  echo " --  generating self signed cert"
   openssl req -x509 -newkey rsa:4086 \
   -subj "/C=XX/ST=XXXX/L=XXXX/O=XXXX/CN=localhost" \
   -keyout "/etc/nginx/external/key.pem" \
   -out "/etc/nginx/external/cert.pem" \
-  -days 3650 -nodes -sha256
+  -days 3650 -nodes -sha256 && \
+  cp /etc/nginx/external/*.conf /etc/nginx/conf.d/ 2> /dev/null > /dev/null && \
+  echo " --  starting nginx"
+  exec nginx -g "daemon off;"
+else
+  echo " --  only copy /etc/nginx/external/*.conf files to /etc/nginx/conf.d/"
+  cp /etc/nginx/external/*.conf /etc/nginx/conf.d/ 2> /dev/null > /dev/null && \
+  echo " --  starting nginx"
+  exec nginx -g "daemon off;"
 fi
 
-echo ">> copy /etc/nginx/external/*.conf files to /etc/nginx/conf.d/"
-cp /etc/nginx/external/*.conf /etc/nginx/conf.d/ 2> /dev/null > /dev/null
 
-exec nginx -g "daemon off;"
+
+
